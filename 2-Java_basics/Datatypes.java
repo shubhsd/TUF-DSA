205e1b5b310187f8da2bfad52a5d715a5f55c79a
Shubham <shubhsd@users.noreply.github.com>
2026-02-21 16:03:25 +0530

    Operators in java

---
+++ 2-Java_basics/Datatypes.java
@@ -0,0 +1,21 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        byte num = 100;
+        short num1 = 1000;
+        int num2 = 653;
+        long num3 = 26372816; 
+        float num4 = (float) 4.7;
+        double num5 = 676.23617863;
+        char character = 'a';
+        boolean val = true;
+        
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(character);
+        System.out.println(val);
+    }
+}
