be80c882dd4b8e441709a79da1d03f418fa29976
Shubham <shubhsd@users.noreply.github.com>
2026-02-21 16:10:34 +0530

    OPerators in java

---
+++ 2-Java_basics/Operators.java
@@ -0,0 +1,17 @@
+public class Operators {
+    public static void main(String[] args) {
+        // 1. Arithematic operators -> '+, - , /, *, %'
+        int num1 = 5;
+        int num2 = 6;
+        int num3 = num1 + num2;
+        int num4 = num1 - num2;
+        int num5 = num1 / num2;
+        int num6 = num1 * num2;
+        int num7 = num1 % num2;
+        System.out.println(num6);
+        System.out.println(num7);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+    }
+}
