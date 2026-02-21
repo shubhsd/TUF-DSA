92900892480c1a7d00b85db7ae804a6b6eaea6db
Shubham <shubhsd@users.noreply.github.com>
2026-02-21 18:52:25 +0530

    Arrays java

---
+++ 2-Java_basics/Arrays.java
@@ -0,0 +1,13 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // Declaring array of let's say size 5
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+        System.out.println(arr[3]);
+        System.out.print(arr.length + " <- Length of given array");
+    }
+}
