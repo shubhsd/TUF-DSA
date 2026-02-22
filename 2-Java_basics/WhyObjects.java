dcc3025778a278fdb9b3504a5a9fc4f532187642
Shubham <shubhsd@users.noreply.github.com>
2026-02-22 16:32:35 +0530

    why objects

---
+++ 2-Java_basics/WhyObjects.java
@@ -0,0 +1,23 @@
+class Test {
+    int age;
+    public void assignAge(int num) {
+        age = num;
+    }
+}
+
+public class WhyObjects {
+    public static void main(String[] args) {
+        Test test1 = new Test();
+        test1.assignAge(10);
+        Test test2 = new Test();
+        test2.assignAge(19);
+
+        System.out.println(test1.age);
+        System.out.println(test2.age);
+    }    
+}
+
+// Here, we created 2 different instances of Test class.
+// So any work done on one instance does not effect the other instance
+
+
