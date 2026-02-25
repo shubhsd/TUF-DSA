b483214ad69c161a801112c93ca4deef3f835136
Shubham <shubhsd@users.noreply.github.com>
2026-02-25 19:37:16 +0530

    IF ELSE IF assignment problem

---
+++ 2-Java_basics/IfElseIf.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+class Solution {
+    // Function to print the grades based on marks
+    public void studentGrade(int marks) {
+        
+        // If else ladder
+        if (marks >= 90) {
+            System.out.print("Grade A");
+        } else if (marks >= 70) {
+            System.out.print("Grade B");
+        } else if (marks >= 50) {
+            System.out.print("Grade C");
+        } else if (marks >= 35) {
+            System.out.print("Grade D");
+        } else {
+            System.out.print("Fail");
+        }
+    }
+}
+
+class IfElseIf {
+    public static void main(String[] args) {
+        // Creating an instance of Solution class
+        Solution solution = new Solution();
+        int marks;
+
+        // Taking marks as input from user
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your marks: ");
+        marks = sc.nextInt();
+
+        // Function call to print the grades based on marks
+        solution.studentGrade(marks);
+    }
+}
