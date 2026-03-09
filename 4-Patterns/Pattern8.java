21bf38f5c824c5d3116bd2e5226ac18edfcca740
Shubham <shubhsd@users.noreply.github.com>
2026-03-09 21:07:31 +0530

    Inverted triangle pattern

---
+++ 4-Patterns/Pattern8.java
@@ -0,0 +1,33 @@
+
+class Pattern8 {
+    // Function to print pattern8
+    public static void pattern8(int n) {
+        
+        // Outer loop which will loop for the rows.
+        for (int i = 0; i < n; i++) {
+            
+            //This loop will print the spaces
+            for(int j = 0; j < i; j++){
+                System.out.print(" ");
+            }
+            
+            // This loop will print asterisk.
+            for (int j = 0; j < 2*n-(2*i+1); j++) {
+                System.out.print("*");
+            }
+            
+            /* As soon as stars for each iteration are printed,
+             move to the next row and give a line break */
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int N = 5;
+
+        // Create an instance of Solution class
+        Pattern8 sol = new Pattern8();
+        sol.pattern8(N);
+    }
+}
+
